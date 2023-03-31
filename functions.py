7b40b530ac67997e14226d5278edd4cfe37c77d1
domsn99 <dominik.hornof@gmail.com>
2023-03-31 02:16:01 +0200

    tkinter integration

--- functions.py
+++ functions.py
@@ -1,16 +1,26 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
+from tkinter import filedialog
+from tkinter import *
+
 from tqdm import tqdm
 from matplotlib.widgets import Slider
 import matplotlib.colors as colors
 from matplotlib.widgets import RangeSlider
 
+data = []
+
+def data_read(dir, typ):
+    data_extracted = []
+    for file in tqdm(os.listdir(dir), desc="Fortschritt"):
+        if file.endswith(typ+".txt"):
+            file_path=os.path.join(dir, file)
+            data_extracted.append(np.loadtxt(file_path))
+    return np.array(data_extracted)
+#ToDo Programm auf tkinter anpassen
 def data_ascii(path, name, type):
     data_path = np.loadtxt(path+name+type,skiprows=1, dtype="uint")
-    return data_path
-def data_raw(index, path, name, type):
-    num="%04d"%index
-    data_path = np.loadtxt(path+name+num+'_ToA'+type,dtype=float)
     return data_path
 def data_file_map (index, path, name, type):
     num="%04d"%index
@@ -79,69 +89,26 @@ def clustering(data, i, k, min_x, min_y, min):
     data[i][k]=0.0
     return data,min_x,min_y,min
 
-def analysis(mode, path, filename, setsize, type):
+def analysis(dir, opt, typ, plot, fig, slider):
+    fig.clear()
+    slider.set(0)
+    global data
+    data=data_read(dir, typ)
 
-    logscale_min=0
-    logscale_max=1
+    if(opt=="roher Datenplot"):
+        map = fig.add_subplot(111)
+        map.imshow(data[0])
+        def plot_update(index):
+            fig.clear()
+            map = fig.add_subplot(111)
+            map.imshow(data[int(index)])
+            plot.draw()
+        slider.configure(to=len(data)-1, command=plot_update)
 
-    if (mode==0):
-        data_plot=data_raw(0, path, filename, type)
-        fig = plt.imshow(data_plot, cmap="hot", norm=colors.SymLogNorm(linthresh=0.03, linscale=0.03,
-                                              vmin=logscale_min, vmax=logscale_max, base=10))
-        plt.colorbar()
-        slider=Slider(ax=plt.axes([0.2, 0, 0.65, 0.03]), label="Data index", valmin=0, valmax=setsize-1, valinit=0, valstep=1)
-
-        def update(index):
-            data=data_raw(index, path, filename, type)
-            fig.set_data(data)
-        
-        slider.on_changed(update)
+    if(opt=="Geclusterter Datenplot"):
+        fig.add_subplot(111).plot(np.sin(range(60)))
     
-    elif (mode == 1):
-        data_map = []
-        for i in tqdm (range(setsize), desc = "Files analyzed: "):
-            data_map.append(data_file_map(i,path,filename, type)) 
-        plt.imshow(data_map, cmap='hot', vmin=np.min(data_map), vmax=np.max(data_map))
-        plt.colorbar()
-        plt.ylabel("Frame [0.1s]")
-        plt.xlabel("Recorded Time Delay (min: "+str(np.min(data_map))+" ns, max: "+str(np.max(data_map))+" ns)")
-    
-    elif (mode == 2):
-        data_hist = []
-        for i in tqdm (range(setsize), desc = "Files analyzed: "):
-            data_hist.append(data_file_hist(i,path,filename, type))
-        data_flattened=np.hstack(data_hist)
-        plt.hist(data_flattened, bins='auto', range=(0.0,100.0))
-        plt.ylabel("Ct/bin")
-        plt.xlabel("Time bin ["+str(np.min(data_flattened[data_flattened != 0]))+" ns]")
-    
-    elif (mode == 3):
-        data_clustered=data_raw(0,path,filename, type)
-        data_clustered=cluster(data_clustered)
-        fig = plt.imshow(data_clustered, cmap="hot", norm=colors.SymLogNorm(linthresh=0.03, linscale=0.03,
-                                              vmin=logscale_min, vmax=logscale_max, base=10))
-        plt.colorbar()
-        slider=Slider(ax=plt.axes([0.2, 0, 0.65, 0.03]), label="Data index", valmin=0, valmax=setsize-1, valinit=0, valstep=1)
+    plot.draw()
 
-        def update(index):
-            fig.set_data(cluster(data_raw(index, path, filename, type)))
-        
-        slider.on_changed(update)
-
-    elif (mode == 4):
-        data_histclust = []
-        for i in tqdm (range(setsize), desc = "Files analyzed: "):
-            data_histclust.append(data_file_hist_clust(i,path,filename, type))
-        data_flattened=np.hstack(data_histclust)
-        plt.hist(data_flattened, bins='auto', range=(0.0,100.0))
-        plt.ylabel("Ct/bin")
-        plt.xlabel("Time bin ["+str(np.min(data_flattened[data_flattened != 0]))+" ns]")
-
-    elif (mode == 5):
-        data_events=data_ascii(path,filename, type)
-        print(data_events.shape)
-        print(data_events)
-        
-        #slider = RangeSlider(ax=plt.axes([0.2, 0, 0.65, 0.03]), label="Data index")
-
-    plt.show()
+def data_length():
+    return len(data)
--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 from functions import *
+from tkinter import *
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
+NavigationToolbar2Tk)
+from tkinter import filedialog
 
-#Modes: 1 ... Data plot with slider
-#       2 ... Histogram
-#       3 ... Clustered data plot with slider
-#       4 ... Histogram of clustered data
-#       5 ... Event hits
+def explorer():
+    global directory
+    directory = filedialog.askdirectory()
+    label_directory.configure(text="Aktueller Wissenspfad: "+directory)
 
-mode = 5
+window = Tk()
+window.title('Zentrum des Wissens')
+window.geometry("1000x1000")
 
-#path="C:\\Users\\domin\\Desktop\\Dataset"
-#path="C:\\Users\\domin\\OneDrive\\Desktop\\Dataset 1\\"
-#filename="first_try_"
-setsize=100
-#datatype=".txt"
+directory = StringVar()
+label_directory = Label(window, text="Das Zentrum des Wissens", width=100, height=4, fg="blue")
 
-path="C:\\Users\\domin\\OneDrive\\Desktop\\Dataset 2\\"
-filename = "Pixel Output Test 2 ASCII first 10000 hits"
-datatype=".t3pa"
+figure = Figure(figsize=(8,8), dpi=100)
+canvas = FigureCanvasTkAgg(figure, master=window)
+canvas.draw()
 
-analysis(mode, path, filename, setsize, datatype)
+button_explorer=Button(master=window, command=explorer, height=2, width=10, text="Ordnerpfad")
+
+selection_plot=StringVar()
+selection_plot.set("Wähle Datenanalyse")
+selection_data=StringVar()
+selection_data.set("Datentyp")
+
+menu_analysis=OptionMenu(window, selection_plot, "roher Datenplot", "Geclusterter Datenplot")
+menu_type=OptionMenu(window, selection_data, "ToA", "ToT")
+
+button_analysis=Button(master=window, command=lambda: analysis(directory, selection_plot.get(), selection_data.get(), canvas, figure, slider_file), height=2, width=10, text="Analyse")
+
+slider_file=Scale(window, from_=0, to=0, orient="horizontal", width=20)
+
+label_directory.grid(column=1, row=1)
+button_explorer.grid(column=2, row=1)
+menu_analysis.grid(column=1, row=2)
+menu_type.grid(column=2, row=2)
+button_analysis.grid(column=1, row=3)
+canvas.get_tk_widget().grid(column=1, row=4, columnspan=2)
+slider_file.grid(column=1, row=5, sticky='we')
+
+window.mainloop()
