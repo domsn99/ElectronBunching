cbd356482b144302b6b83e3f75c7698536b4405c
domsn99 <dominik.hornof@gmail.com>
2023-04-08 11:28:50 +0200

    better gui

--- main.py
+++ main.py
@@ -12,14 +12,16 @@ def explorer():
 
 window = Tk()
 window.title('Zentrum des Wissens')
-window.geometry("1000x1000")
+window.geometry("700x700")
 
 directory = StringVar()
 label_directory = Label(window, text="Das Zentrum des Wissens", width=100, height=4, fg="blue")
 
-figure = Figure(figsize=(8,8), dpi=100)
+figure = Figure()
 canvas = FigureCanvasTkAgg(figure, master=window)
 canvas.draw()
+
+toolbar = NavigationToolbar2Tk(canvas, window)
 
 button_explorer=Button(master=window, command=explorer, height=2, width=10, text="Ordnerpfad")
 
@@ -33,14 +35,15 @@ menu_type=OptionMenu(window, selection_data, "ToA", "ToT")
 
 button_analysis=Button(master=window, command=lambda: analysis(directory, selection_plot.get(), selection_data.get(), canvas, figure, slider_file), height=2, width=10, text="Analyse")
 
-slider_file=Scale(window, from_=0, to=0, orient="horizontal", width=20)
+slider_file=Scale(window, from_=0, to=0, orient="horizontal", width=20, length=500)
 
-label_directory.grid(column=1, row=1)
-button_explorer.grid(column=2, row=1)
-menu_analysis.grid(column=1, row=2)
-menu_type.grid(column=2, row=2)
-button_analysis.grid(column=1, row=3)
-canvas.get_tk_widget().grid(column=1, row=4, columnspan=2)
-slider_file.grid(column=1, row=5, sticky='we')
+label_directory.pack(side="top")
+canvas.get_tk_widget().pack(side=BOTTOM)
+slider_file.pack(side="bottom")
+button_explorer.pack(side=LEFT)
+menu_analysis.pack(side=LEFT)
+menu_type.pack(side=LEFT)
+button_analysis.pack(side=LEFT)
+toolbar.update()
 
 window.mainloop()
